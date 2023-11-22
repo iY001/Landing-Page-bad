705f580432fbbcf64d1d8b8c97c904f8dec859bc
iY001 <youssefabdo4005@gmail.com>
2023-11-22 21:09:18 +0200

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
-import Content from './components/Content';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Reviews from './components/Reviews';
-import Footer from './components/footer'
+import Home from './Pages/Home';
+
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Header />
-      <Content/>
-      <Blog/>
-      <Reviews/>
-      <Contact/>
-      <Footer/>
+
+    <Home />
     </>
   );
 }
