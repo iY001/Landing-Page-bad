9ec785270effcb98cc55e8c3428b49ed1bac562c
iY001 <youssefabdo4005@gmail.com>
2023-11-23 22:24:57 +0200

    SOLVE #2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Blog from '../components/Blog';
-import Contact from '../components/Contact';
-import Content from '../components/Content';
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
-import Reviews from '../components/Reviews';
-import Footer from '../components/footer';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Content from './components/Content';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Reviews from './components/Reviews';
+import Footer from './components/footer';
 
 function App() {
   return (
